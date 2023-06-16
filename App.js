d67b3d29906b2d96d57120e4f3a670930e98766b
Mootbing <50122069+Mootbing@users.noreply.github.com>
2023-06-16 15:46:11 -0400

    works

--- App.js
+++ App.js
@@ -1,17 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
+import { rtdb } from './src/firebase_auth';
 
 export default function App() {
 
   const [count, setCount] = useState(0);
   const [offline, setOffline] = useState(false);
 
+  useEffect(() => {
+    rtdb.ref("count").on("value", (snapshot) => {
+      setCount(snapshot.val());
+    })
+  }, [])
+  
+  useEffect(() => {
+    rtdb.ref("count").set(count);
+  }, [count])
+
+  useEffect(() => {
+    if (offline) {
+      rtdb.goOffline();
+    } else {
+      rtdb.goOnline();
+    }
+  }, [offline])
+  
   return (
     <View style={styles.container}>
       <View style={{flexDirection: "row"}}>
         <Button title="+" onPress={() => setCount(count + 1)} />
-        <Text style={{marginLeft: 25, marginRight: 25}}>Count: {count}</Text>
+        <Text style={{marginLeft: 25, marginRight: 25, color: "white", fontSize: 30}}>{count}</Text>
         <Button title="-" onPress={() => setCount(count - 1)} />
       </View>
       <Button title={offline ? "go online (ofline rn)" : "go offline (online rn)"} onPress={() => setOffline(!offline)} />
@@ -22,7 +41,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
