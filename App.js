23802bec27344c469e4b4c4a01e699ac92534386
Mootbing <50122069+Mootbing@users.noreply.github.com>
2023-06-16 14:59:26 -0400

    base code

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [count, setCount] = useState(0);
+  const [offline, setOffline] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{flexDirection: "row"}}>
+        <Button title="+" onPress={() => setCount(count + 1)} />
+        <Text style={{marginLeft: 25, marginRight: 25}}>Count: {count}</Text>
+        <Button title="-" onPress={() => setCount(count - 1)} />
+      </View>
+      <Button title={offline ? "go online (ofline rn)" : "go offline (online rn)"} onPress={() => setOffline(!offline)} />
     </View>
   );
 }
